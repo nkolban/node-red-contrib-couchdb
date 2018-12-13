fc27a3b3db296fa83978d0aeb05565c9806ddb26
Connor <connor.aitken@gmail.com>
2018-12-12 20:31:43 -0800

    not needed
    you only need this if you are the on authors windows system.

--- install.bat
+++
@@ -1,3 +0,0 @@
-copy package.json C:\Users\kolban\.node-red\node_modules\node-red-contrib-couchdb
-copy couchdb.html C:\Users\kolban\.node-red\node_modules\node-red-contrib-couchdb
-copy couchdb.js C:\Users\kolban\.node-red\node_modules\node-red-contrib-couchdb
